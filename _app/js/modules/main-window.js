12ae9831bceb13e0cef2f39c80b858f437bdfb2a
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-05-03 19:51:36 +0200

    added render error message

--- _app/js/modules/main-window.js
+++ _app/js/modules/main-window.js
@@ -5,6 +5,7 @@ import formatSeconds from '../util/format-seconds.js';
 import playerModule from './player.js';
 
 export default async function mainWindow() {
+	let errorMessage = '';
 	let releases = await fetchAllReleases();
 
 	const player = playerModule(releases);
@@ -45,8 +46,15 @@ export default async function mainWindow() {
 			},
       }`;
       
-      const fetchedReleases = await sanity.fetch(query);
-      return fetchedReleases;
+		const fetchedReleases = await sanity.fetch(query);
+		const isError = typeof fetchedReleases !== 'string';
+		
+		if (isError) {
+			return fetchedReleases;
+		} else {
+			errorMessage = fetchedReleases;
+			return [];
+		}
    }
 
 	renderHTML();
@@ -64,136 +72,149 @@ export default async function mainWindow() {
 
 		mainWindow.innerHTML = '';
 
-		releases.forEach((release, index) => {
+		if (errorMessage) {
 			const container = document.createElement('div');
-			const releaseContainer = createReleaseDOM();
-			const songHeaderContainer = createSongHeader();
-			const songsContainer = createSongsDOM();
+			const message = document.createElement('div');
 
-			container.dataset.releaseNumber = index;
-			
-			container.className = 'release';
-			songsContainer.className = 'release__songs';
-			
-			container.append(releaseContainer);
-			container.append(songHeaderContainer);
-			container.append(songsContainer);
+			message.innerText = errorMessage;
 
-			function createSongHeader() {
-				const songHeaderContainer = document.createElement('div');
-				const number = document.createElement('div');
-				const title = document.createElement('div');
-				const plays = document.createElement('div');
-				const time = document.createElement('div');
+			container.className = 'error';
+			message.className = 'error__message';
 
-				songHeaderContainer.className = 'release__song-header';
-
-				number.innerText = '#';
-				title.innerText = 'title';
-				plays.innerText = 'plays';
-				time.innerText = 'time';
-
-				songHeaderContainer.append(number);
-				songHeaderContainer.append(title);
-				songHeaderContainer.append(plays);
-				songHeaderContainer.append(time);
-
-				return songHeaderContainer;
-			}
-
-			function createReleaseDOM() {
-				const totalSecondsOfRelease = reduceTotalPlayTimeOfTracks(release.tracks)
-
-				const releaseContainer = document.createElement('div');
-				const artworkContainer = document.createElement('div');
-				const artwork = document.createElement('img');
-				const metaDataContainer = document.createElement('div');
-				const title = document.createElement('div');
-				const artist = document.createElement('div');
-				const releaseType = document.createElement('div');
-				const moreMetaDataContainer = document.createElement('div');
-				const releaseDate = document.createElement('div');
-				const amountOfSongs = document.createElement('div');
-				const playTime = document.createElement('div');
-
-				artwork.src = release.tracks[0].artworkURL;
-				artwork.alt = release.tracks[0].artworkAlt;
-
-				title.innerText = release.title;
-				artist.innerText = release.artists.join(', ');
-				releaseType.innerText = release.type;
-				releaseDate.innerText = formatDate(release.releaseDate);
-				amountOfSongs.innerText = `${release.tracks.length} songs`;
-				playTime.innerText = formatSeconds(totalSecondsOfRelease);
-
-				releaseContainer.className = 'release__release-container';
-				metaDataContainer.className = 'release__meta-data-container';
-				moreMetaDataContainer.className = 'release__more-meta-data-container';
-				artworkContainer.className = 'release__artwork'; 
-				title.className = 'release__title';
-				artist.className = 'release__artist';
-				releaseType.className = 'release__type';
-
-				artworkContainer.append(artwork);
-				releaseContainer.append(artworkContainer);
-
-				moreMetaDataContainer.append(releaseDate);
-				moreMetaDataContainer.append(amountOfSongs);
-				moreMetaDataContainer.append(playTime);
-
-				metaDataContainer.append(title);
-				metaDataContainer.append(artist);
-				metaDataContainer.append(releaseType);
-				metaDataContainer.append(moreMetaDataContainer);
-
-				releaseContainer.append(metaDataContainer);
-
-				return releaseContainer;
-			}
-
-			function createSongsDOM() {
-				const songsContainer = document.createElement('div');
+			container.append(message);
+			mainWindow.append(container)
+		} else {
+			releases.forEach((release, index) => {
+				const container = document.createElement('div');
+				const releaseContainer = createReleaseDOM();
+				const songHeaderContainer = createSongHeader();
+				const songsContainer = createSongsDOM();
 	
-				release.tracks.forEach((track, index) => {
-					const formattedPlaytime = `${track.playTime.minutes.toString().padStart(2, '0')}:${track.playTime.seconds.toString().padStart(2, '0')}`;
-					const formattedPlays = formatPlays(String(track.plays));
-
-					const songContainer = document.createElement('button');
+				container.dataset.releaseNumber = index;
+				
+				container.className = 'release';
+				songsContainer.className = 'release__songs';
+				
+				container.append(releaseContainer);
+				container.append(songHeaderContainer);
+				container.append(songsContainer);
+	
+				function createSongHeader() {
+					const songHeaderContainer = document.createElement('div');
 					const number = document.createElement('div');
-					const titleArtistContainer = document.createElement('div');
 					const title = document.createElement('div');
-					const artist = document.createElement('div');
 					const plays = document.createElement('div');
 					const time = document.createElement('div');
-
-					songContainer.dataset.trackNumber = index;
-
-					number.innerText = index + 1;
-					title.innerText = track.title;
-					artist.innerText = track.artists.join(', ');
-					plays.innerText = formattedPlays;
-					time.innerText = formattedPlaytime;
-
-					songContainer.className = 'release__song';
-					number.className = 'release__number';
-					title.className = 'release__track-title';
-
-					titleArtistContainer.append(title);
-					titleArtistContainer.append(artist);
-
-					songContainer.append(number);
-					songContainer.append(titleArtistContainer);
-					songContainer.append(plays);
-					songContainer.append(time);
-
-					songsContainer.append(songContainer);
-				});
-
-				return songsContainer;
-			}
-
-			mainWindow.append(container);
-		});
+	
+					songHeaderContainer.className = 'release__song-header';
+	
+					number.innerText = '#';
+					title.innerText = 'title';
+					plays.innerText = 'plays';
+					time.innerText = 'time';
+	
+					songHeaderContainer.append(number);
+					songHeaderContainer.append(title);
+					songHeaderContainer.append(plays);
+					songHeaderContainer.append(time);
+	
+					return songHeaderContainer;
+				}
+	
+				function createReleaseDOM() {
+					const totalSecondsOfRelease = reduceTotalPlayTimeOfTracks(release.tracks)
+	
+					const releaseContainer = document.createElement('div');
+					const artworkContainer = document.createElement('div');
+					const artwork = document.createElement('img');
+					const metaDataContainer = document.createElement('div');
+					const title = document.createElement('div');
+					const artist = document.createElement('div');
+					const releaseType = document.createElement('div');
+					const moreMetaDataContainer = document.createElement('div');
+					const releaseDate = document.createElement('div');
+					const amountOfSongs = document.createElement('div');
+					const playTime = document.createElement('div');
+	
+					artwork.src = release.tracks[0].artworkURL;
+					artwork.alt = release.tracks[0].artworkAlt;
+	
+					title.innerText = release.title;
+					artist.innerText = release.artists.join(', ');
+					releaseType.innerText = release.type;
+					releaseDate.innerText = formatDate(release.releaseDate);
+					amountOfSongs.innerText = `${release.tracks.length} songs`;
+					playTime.innerText = formatSeconds(totalSecondsOfRelease);
+	
+					releaseContainer.className = 'release__release-container';
+					metaDataContainer.className = 'release__meta-data-container';
+					moreMetaDataContainer.className = 'release__more-meta-data-container';
+					artworkContainer.className = 'release__artwork'; 
+					title.className = 'release__title';
+					artist.className = 'release__artist';
+					releaseType.className = 'release__type';
+	
+					artworkContainer.append(artwork);
+					releaseContainer.append(artworkContainer);
+	
+					moreMetaDataContainer.append(releaseDate);
+					moreMetaDataContainer.append(amountOfSongs);
+					moreMetaDataContainer.append(playTime);
+	
+					metaDataContainer.append(title);
+					metaDataContainer.append(artist);
+					metaDataContainer.append(releaseType);
+					metaDataContainer.append(moreMetaDataContainer);
+	
+					releaseContainer.append(metaDataContainer);
+	
+					return releaseContainer;
+				}
+	
+				function createSongsDOM() {
+					const songsContainer = document.createElement('div');
+		
+					release.tracks.forEach((track, index) => {
+						const formattedPlaytime = `${track.playTime.minutes.toString().padStart(2, '0')}:${track.playTime.seconds.toString().padStart(2, '0')}`;
+						const formattedPlays = formatPlays(String(track.plays));
+	
+						const songContainer = document.createElement('button');
+						const number = document.createElement('div');
+						const titleArtistContainer = document.createElement('div');
+						const title = document.createElement('div');
+						const artist = document.createElement('div');
+						const plays = document.createElement('div');
+						const time = document.createElement('div');
+	
+						songContainer.dataset.trackNumber = index;
+	
+						number.innerText = index + 1;
+						title.innerText = track.title;
+						artist.innerText = track.artists.join(', ');
+						plays.innerText = formattedPlays;
+						time.innerText = formattedPlaytime;
+	
+						songContainer.className = 'release__song';
+						number.className = 'release__number';
+						title.className = 'release__track-title';
+	
+						titleArtistContainer.append(title);
+						titleArtistContainer.append(artist);
+	
+						songContainer.append(number);
+						songContainer.append(titleArtistContainer);
+						songContainer.append(plays);
+						songContainer.append(time);
+	
+						songsContainer.append(songContainer);
+					});
+	
+					return songsContainer;
+				}
+	
+				mainWindow.append(container);
+			});
+		}
 
 		songsEl = document.querySelectorAll('.release__song');
 
