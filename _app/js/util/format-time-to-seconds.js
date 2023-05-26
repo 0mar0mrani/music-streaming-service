5017fa4c1f3d61f8e03a765d156c53a9984ec225
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-05-26 21:02:47 +0200

    added documentation to format-time-to-seconds

--- _app/js/util/format-time-to-seconds.js
+++ _app/js/util/format-time-to-seconds.js
@@ -1,3 +1,15 @@
+/**
+ * This function takes an array of tracks and returns the total play time in seconds.
+ * It iterates over tracks and reduce playtime to to second by using reduce method.
+ * @param {object[]} tracks - An array of tracks
+ * @returns {number} totalSeconds - The total play time in seconds
+ * @example
+ * const tracks = [
+ *	{ playTime: { minutes: 3, seconds: 20 } },
+ *	{ playTime: { minutes: 4, seconds: 10 } }
+ * ]
+ * formatTimeToSeconds(tracks) // returns 300;
+ */
 export default function formatTimeToSeconds(tracks) {
 	const totalSeconds = tracks.reduce((accumulator, track) => {
 		return accumulator + track.playTime.minutes * 60 + track.playTime.seconds
