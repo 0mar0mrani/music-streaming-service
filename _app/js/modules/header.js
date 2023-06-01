b4f3e6af7312c77f285e2ecd7bd88a03b1b0e80f
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-06-01 11:23:38 +0200

    added documentation

--- _app/js/modules/header.js
+++ _app/js/modules/header.js
@@ -9,22 +9,43 @@ export default function header() {
 	const messageElement = document.querySelector('.header__message');
 	const createPlaylistButton = document.querySelector('.header__create-playlist-button');
 
+	/**
+	 * Sets message, this function was made to be used by main-window.js.
+	 * @see main-window.js
+	 * @param {string} string - The string to set message
+	 */
 	function setMessage(string) {
 		message = string;
 	}
 
+	/**
+	 * Sets if message should be visible, this function was made to be used by main-window.js.
+	 * @see main-window.js
+	 * @param {boolean} boolean - To set if true or false
+	 */
 	function setIsMessageVisible(boolean) {
 		isMessageVisible = boolean;
 	}
 
+	/**
+	 * Sets currentSection, this function was made to be used by main-window.js.
+	 * @see main-window.js
+	 * @param {string} string - The string to set currentSection
+	 */
 	function setCurrentSection(string) {
 		currentSection = string;
 	}
 
+	/**
+	 * This main function consist of subfunctions that renders the HTML based on the state of header. 
+	 */
 	function renderHTML() {		
 		renderMessage();
 		renderTitleAndButton();
 
+		/**
+		 * Renders message if isMessageVisible is set to true, and removes message with setTimeout after the duration of messageDuration. 
+		 */
 		function renderMessage() {
 			if (isMessageVisible) {
 				messageElement.innerText = message;
@@ -40,6 +61,9 @@ export default function header() {
 			}
 		}
 
+		/**
+		 * Renders title and button in header. 
+		 */
 		function renderTitleAndButton() {
 			if (currentSection === 'release') {
 				headerTitleElement.innerText = 'Releases';
